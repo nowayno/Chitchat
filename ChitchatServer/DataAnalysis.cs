1b56ed853afbcf081d346bc3b32d26c6bb2131d0
nowayno <yangfeng666@sina.cn>
2016-08-05 14:57:55 +0800

    server

---
+++ ChitchatServer/DataAnalysis.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChitchatServer
+{
+    class DataAnalysis
+    {
+        static public List<string> dataAna(byte[] data)
+        {
+            string dataString = Encoding.Default.GetString(data);
+            List<string> result = new List<string>();
+
+            return result;
+        }
+    }
+}
